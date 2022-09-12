70770b0bee480764d9ad376f62f7ac969e732a68
DayneLeon <dayneleon94@gmail.com>
2022-09-12 14:39:51 -0300

    septiembre git commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var boton=getElementById("boton");
+    formulario=getElementById("formulario")
+
+boton.addEventListener("click",iniciar_sesion,true)
+
+function iniciar_sesion(){
+    return formulario
+}
